87634b3ad0ce526a4cd007e45612cdbf37989d35
Michael Joyner <joyner.michael@gmail.com>
2024-04-15 09:23:38 +0800

    setup

---
+++ netlify/functions/record-logs.ts
@@ -0,0 +1,12 @@
+import type { Config } from "@netlify/functions";
+
+export default async (req: Request) => {
+  const { next_run } = await req.json();
+
+  console.log(process.env.AQI_TOKEN);
+  console.log("Received event! Next invocation at:", next_run);
+};
+
+export const config: Config = {
+  schedule: "@hourly",
+};
