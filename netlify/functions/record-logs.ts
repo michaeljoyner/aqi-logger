c2ae51de8d1a95df0fce0fea400981e8912d37fb
Michael Joyner <joyner.michael@gmail.com>
2024-04-27 12:16:34 +0800

    log error

--- netlify/functions/record-logs.ts
+++ netlify/functions/record-logs.ts
@@ -4,29 +4,37 @@ import type { TursoCreds, City } from "../../types";
 
 export default async (req: Request) => {
   const creds: TursoCreds = {
-    url: process.env.TURSO_DATABASE_URL || '',
-    authToken: process.env.TURSO_AUTH_TOKEN || '',
+    url: process.env.TURSO_DATABASE_URL || "",
+    authToken: process.env.TURSO_AUTH_TOKEN || "",
   };
 
-const aqiToken = process.env.AQI_TOKEN;
+  const aqiToken = process.env.AQI_TOKEN;
 
   const client = createClient(creds);
 
   const resp = await client.execute("SELECT * FROM cities");
 
-  const cities: City[] = resp.rows.map((row) => ({...row})); 
-  
+  const cities: City[] = resp.rows.map((row) => ({ ...row }));
+
   cities.forEach(async (city) => {
     try {
-      const resp = await fetch(`https://api.waqi.info/feed/geo:${city.lat};${city.lng}/?token=${aqiToken}`);
+      const resp = await fetch(
+        `https://api.waqi.info/feed/geo:${city.lat};${city.lng}/?token=${aqiToken}`,
+      );
       const respData = await resp.json();
       await client.execute({
         sql: "INSERT INTO records (city_id, hour, value) VALUES (?, ?, ?)",
-        args: [city.id, new Date(respData.data.time.s).getHours(), respData.data.aqi]
+        args: [
+          city.id,
+          new Date(respData.data.time.s).getHours(),
+          respData.data.aqi,
+        ],
       });
-    } catch(err) {}
+    } catch (err) {
+      console.log(err);
+    }
   });
-}
+};
 
 export const config: Config = {
   schedule: "@hourly",
